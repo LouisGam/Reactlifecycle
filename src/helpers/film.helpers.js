59d3ffd7991f61cedafcf1159a33c17b3c955291
Louis Gambardella <LouisGambardella@gmail.com>
2024-05-03 13:36:05 -0700

    Finished getfilmsstats and part 2 of exercise

--- src/helpers/film.helpers.js
+++ src/helpers/film.helpers.js
@@ -1,8 +1,8 @@
 export function filterFilmsByDirector(list, director) {
-    if(director == "") {
-        return list;
-    }
-    return list.filter((movie) => movie.director == director)
+  if (director == "") {
+    return list;
+  }
+  return list.filter((movie) => movie.director == director);
 }
 
 export function getListOf(list, prop) {
@@ -23,5 +23,32 @@ export function getListOf(list, prop) {
   return lastNameArray;
 }
 export function getFilmStats(list) {
-return {avg_score: 0, acc_score: 0, total: 0, latest: 0};
+
+  if (list.length <= 0) {
+    return {
+      avg_score: 0,
+      acc_score: 0,
+      total: 0,
+      latest: 1600,
+    };
+  }
+
+  const acc_score = list.reduce((sum, film) => {
+    return sum + Number(film.rt_score);
+  }, 0);
+
+  const avg_score = acc_score / list.length;
+  
+  const descendingSortedReleasesArray = list.sort((a, b) => {
+    return b.release_date - a.release_date;
+  });
+
+  const latestReleaseDate = descendingSortedReleasesArray[0].release_date;
+
+  return {
+    avg_score: avg_score,
+    acc_score: acc_score,
+    total: list.length,
+    latest: latestReleaseDate,
+  };
 }
--- src/pages/films.page.jsx
+++ src/pages/films.page.jsx
@@ -3,28 +3,28 @@ import "./films.page.jsx";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { filterFilmsByDirector, getListOf } from "../helpers/film.helpers.js";
+import { getFilmStats } from "../helpers/film.helpers.js";
 
 function FilmsPage() {
-
   const [films, setFilms] = useState([]);
-    const [searchDirector, setSearchDirector] = useState("");
+  const [searchDirector, setSearchDirector] = useState("");
 
-    useEffect(() => {
-      fetch(`https://studioghibliapi-d6fc8.web.app/films`)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data);
-          setFilms(data);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }, []);
+  useEffect(() => {
+    fetch(`https://studioghibliapi-d6fc8.web.app/films`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setFilms(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-    const  filteredFilms = filterFilmsByDirector(films, searchDirector);
-    const allDirectors = getListOf(films, "director");
+  const filteredFilms = filterFilmsByDirector(films, searchDirector);
+  const allDirectors = getListOf(films, "director");
+  const { avg_score, latest, total } = getFilmStats(filteredFilms);
 
 
   return (
@@ -32,28 +32,46 @@ function FilmsPage() {
       <h1>Studio Films List</h1>
       <form>
         <div className="form-group">
-          <label htmlFor="searchDirector"></label>  
-          <select 
-          value={searchDirector}
-          name="serachDirector"
+          <label htmlFor="searchDirector"></label>
+          <select
+            value={searchDirector}
+            name="serachDirector"
             id="searchDirector"
             onChange={(event) => {
-                setSearchDirector(event.target.value)}}>
+              setSearchDirector(event.target.value);
+            }}
+          >
             <option value="All"></option>
             {allDirectors.map((director) => {
-                return<option value={director}>{director}</option>})}
-              
+              return <option value={director}>{director}</option>;
+            })}
           </select>
         </div>
       </form>
+      <div className="filmStats">
+        <div>
+          <span># Of Films </span>
+          <span>{total}</span>
+        </div>
+        <div>
+          <span>Average Rating </span>
+          <span>{avg_score.toFixed(2)}</span>
+        </div>
+        <div>
+          <span>Latest Film </span>
+          <span>{latest}</span>
+        </div>
+      </div>
       <p>Created By: Louis Gambardella</p>
       <ul>
         {filteredFilms.map((film) => {
           return (
             <li key={film.id}>
               <div className="movie-left">
-              <h2><Link to={`film/${film.id}`}>{film.title}</Link></h2>
-              <img src={film.image} alt={`${film.title} banner`} />
+                <h2>
+                  <Link to={`film/${film.id}`}>{film.title}</Link>
+                </h2>
+                <img src={film.image} alt={`${film.title} banner`} />
               </div>
               <div className="movie-right">
                 <p>{film.description}</p>
